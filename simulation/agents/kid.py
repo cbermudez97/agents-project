988768d6186e9e745023acb7c047f8e2f36afb16
cbermudez <cbermudez.dev@gmail.com>
2020-11-20 12:55:53 -0500

    Implement basic agent for babies behavior

---
+++ simulation/agents/kid.py
@@ -0,0 +1,23 @@
+from random import randint
+
+from .agent import Agent
+from ..environment.room import FreeCell, ObstacleCell
+from ..environment.env_actions import MovNorth, MovEast, MovSouth, MovWest, Hold, dx, dy
+
+
+class KidAgent(Agent):
+    def __init__(self, x, y):
+        super(KidAgent, self).__init__(x,y)
+        self.loaded_by = None
+
+    def loaded(self):
+        return not self.loaded_by is None
+    
+    def  action(self, percept):
+        free = [ dir for dir in [MovNorth, MovEast, MovSouth, MovWest]                           \
+            if percept.valid_pos(self.x + dx[dir], self.y + dy[dir])                             \
+                and not percept.occupy(self.x + dx[dir], self.y + dy[dir])                       \
+                and percept.floor[self.x + dx[dir]][self.y + dy[dir]] in [FreeCell, ObstacleCell]]
+        free.append(Hold)
+        op = randint(0, len(free)-1)
+        return free[op]
