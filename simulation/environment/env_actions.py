5f6ee0765451ed9eabf839428cb5f3abf5bf0976
cbermudez <cbermudez.dev@gmail.com>
2020-11-20 17:04:36 -0500

    Change actions identifiers order

--- simulation/environment/env_actions.py
+++ simulation/environment/env_actions.py
@@ -1,8 +1,8 @@
 # Env Actions
-MovNorth, MovEast, MovSouth, MovWest, MovSEast, MovNWest, MovNEast, MovSWest, MovNorth2, MovEast2, MovSouth2, MovWest2, Hold, Clean, DropKid = range(15)
+MovNorth, MovEast, MovSouth, MovWest, Hold, Clean, DropKid, MovSEast, MovNWest, MovNEast, MovSWest, MovNorth2, MovEast2, MovSouth2, MovWest2 = range(15)
 
 # Directions
-dx = [-1, 0, 1,  0]
-dy = [ 0, 1, 0, -1]
-edx = dx + [1, -1, -1,  1]
-edy = dy + [1, -1,  1, -1]
+dx = [-1, 0, 1,  0, 0, 0, 0, 0]
+dy = [ 0, 1, 0, -1, 0, 0, 0, 0]
+edx = dx + [1, -1, -1,  1, -2, 0, 2,  0]
+edy = dy + [1, -1,  1, -1,  0, 2, 0, -2]
